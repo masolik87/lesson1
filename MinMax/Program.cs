13ba13c5092f2230b97ce462e47cb9438cb2a290
Дмитрий <dimon20.07@mail.ru>
2022-08-31 22:56:01 +0300

    MinMax

---
+++ MinMax/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт,какое
+// число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Enter the number");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+ int max = a;
+ int min = b;
+ {
+    if(a > max) max = a;
+    if (b > max) max = b;
+    if(a < min) min = a;
+    if(b < min) min =b;
+ }
+Console.Write("max = ");
+Console.WriteLine(max); 
+Console.Write("min = ");
+Console.WriteLine(min);
